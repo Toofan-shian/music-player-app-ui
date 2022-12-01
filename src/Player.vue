cc505794907498a287a3332c4848302d9c9fdabe
Toofan <toofan.shian@gmail.com>
2022-12-01 18:02:02 +0330

    add-title-bar

--- src/Player.vue
+++ src/Player.vue
@@ -1,20 +1,30 @@
 <template>
   <v-app>
-   
+    <v-content class="pt-10">
+      <v-container
+        class='secondary darken-1'
+      >
+
+        <titleBar></titleBar>
+
+      </v-container>
+    </v-content>
   </v-app>
 </template>
 
 <script>
+import titleBar from './components/titleBar.vue'
 
 export default {
-  name: 'App',
 
   components: {
-    HelloWorld,
+    titleBar,
   },
 
-  data: () => ({
-    //
-  }),
+  data() {
+    return {
+      
+    }
+  },
 };
 </script>
---
+++ src/components/titleBar.vue
@@ -0,0 +1,85 @@
+<template>
+  <v-system-bar>
+
+    <v-menu offset-y v-model="menu">
+
+      <template v-slot:activator="{ on }">
+        <v-btn
+          v-on="on"
+          text
+          height="100%"
+          plain
+          class="pa-0"
+        >
+          <v-icon>mdi-menu</v-icon>
+          <span>Menu</span>
+        </v-btn>
+      </template>
+
+      <v-list>
+
+        <v-dialog v-model="dialog" width="500px">
+
+          <template v-slot:activator="{ on }">
+            <v-list-item v-on="on" dense>
+              <v-list-item-title>About</v-list-item-title>
+            </v-list-item>
+          </template>
+
+          <v-card>
+            <v-card-title>
+              A music-player app built with vuetify
+            </v-card-title>
+            <v-card-text>
+              version-1
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn outlined
+                @click="closeDialogAndMenu"
+              >
+              Ok
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+
+
+      </v-list>
+    </v-menu>
+
+    <v-spacer></v-spacer>
+
+    <h3 class="app-name fw-bold">Musify</h3>
+
+    <v-spacer></v-spacer>
+
+    <v-icon>mdi-window-minimize</v-icon>
+    <v-icon>mdi-window-maximize</v-icon>
+    <v-icon>mdi-close</v-icon>
+  </v-system-bar>
+  
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      dialog: false,
+      menu: false,
+    }
+  },
+  methods: {
+    closeDialogAndMenu() {
+      this.dialog = false;
+      this.menu = false;
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
